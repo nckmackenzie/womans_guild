42638e3f21e6b4a38fd7557beb52d800cd5c51ec
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-08-10 22:35:42 +0300

    refactor: add CreateNewButton

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
+import { Link } from 'react-router-dom';
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
@@ -53,4 +54,15 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 );
 Button.displayName = 'Button';
 
-export { Button, buttonVariants };
+function CreateNewButton({
+  children,
+  href,
+}: React.PropsWithChildren<{ href: string }>) {
+  return (
+    <Button asChild>
+      <Link to={href}>{children}</Link>
+    </Button>
+  );
+}
+
+export { Button, buttonVariants, CreateNewButton };
