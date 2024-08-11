c73f3fb8804120ecdfeb4e42a5dfa5b5e4f131c9
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-08-11 22:24:11 +0300

    chore: remove create user schema

--- client/src/features/login/utils/schema.ts
+++ client/src/features/login/utils/schema.ts
@@ -1,4 +1,3 @@
-import { validateEmail } from '@/lib/utils';
 import { z } from 'zod';
 
 export const loginSchema = z.object({
@@ -12,38 +11,3 @@ export const loginSchema = z.object({
     .trim()
     .min(8, 'Password must be at least 8 characters long'),
 });
-
-export const userFormSchema = z
-  .object({
-    name: z
-      .string()
-      .trim()
-      .min(1, { message: 'Name is required.' })
-      .toLowerCase(),
-    contact: z
-      .string()
-      .min(1, { message: 'Phone number is required.' })
-      .max(10, { message: 'Invalid phone number' }),
-    email: z.string().optional(),
-    password: z.string().min(8, { message: 'Password is required.' }),
-    confirmPassword: z
-      .string()
-      .min(8, { message: 'Confirm password is required.' }),
-    role: z.enum(['admin', 'standard_user']).default('standard_user'),
-  })
-  .superRefine(({ confirmPassword, password, email }, ctx) => {
-    if (confirmPassword !== password) {
-      ctx.addIssue({
-        code: 'custom',
-        message: 'Passwords do not match',
-        path: ['confirmPassword'],
-      });
-    }
-    if (email && !validateEmail(email)) {
-      ctx.addIssue({
-        code: 'custom',
-        message: 'Invalid email address',
-        path: ['email'],
-      });
-    }
-  });
