6e0aa2f3b9e6f9f9c761e5d253006563205c488f
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-08-11 22:24:28 +0300

    chore: remove types form user

--- client/src/features/login/types/index.ts
+++ client/src/features/login/types/index.ts
@@ -1,13 +1,4 @@
 import { z } from 'zod';
-import { loginSchema, userFormSchema } from '../utils/schema';
+import { loginSchema } from '../utils/schema';
 
 export type LoginFormValues = z.infer<typeof loginSchema>;
-export type UserFormValues = z.infer<typeof userFormSchema>;
-export type ReturnedUser = Omit<
-  UserFormValues,
-  'password' | 'confirmPassword'
-> & {
-  created_at: string;
-  updated_at: string;
-  id: number;
-};
