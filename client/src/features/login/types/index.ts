273fc96d1966980b54a6fa2114dda0fe026b3f2b
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-08-11 21:51:41 +0300

    chore: add type for ReturnedUser

--- client/src/features/login/types/index.ts
+++ client/src/features/login/types/index.ts
@@ -1,4 +1,13 @@
 import { z } from 'zod';
-import { loginSchema } from '../utils/schema';
+import { loginSchema, userFormSchema } from '../utils/schema';
 
 export type LoginFormValues = z.infer<typeof loginSchema>;
+export type UserFormValues = z.infer<typeof userFormSchema>;
+export type ReturnedUser = Omit<
+  UserFormValues,
+  'password' | 'confirmPassword'
+> & {
+  created_at: string;
+  updated_at: string;
+  id: number;
+};
