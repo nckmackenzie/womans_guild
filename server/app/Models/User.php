fe5e5ea8b03428beae977c603201a6ac9640edac
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-08-11 21:45:46 +0300

    chore: add columns in fillable

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -20,7 +20,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'contact'
+        'contact',
+        'role',
+        'active'
     ];
 
     /**
