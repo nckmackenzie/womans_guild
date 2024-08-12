ea7c489bd3a09ff530673a3bee0b918d08243d43
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-12 20:39:35 +0300

    refactor: change casing to snake case

--- client/src/features/years/schema.ts
+++ client/src/features/years/schema.ts
@@ -7,11 +7,11 @@ export const yearFormSchema = z
       .trim()
       .min(1, { message: 'Year is required.' })
       .toLowerCase(),
-    startDate: z.coerce.date({ required_error: 'Start date is required' }),
-    endDate: z.coerce.date({ required_error: 'End date is required' }),
+    start_date: z.coerce.date({ required_error: 'Start date is required' }),
+    end_date: z.coerce.date({ required_error: 'End date is required' }),
   })
-  .superRefine(({ startDate, endDate }, ctx) => {
-    if (startDate > endDate) {
+  .superRefine(({ start_date, end_date }, ctx) => {
+    if (start_date > end_date) {
       ctx.addIssue({
         code: 'custom',
         message: 'Start date must be before end date',
