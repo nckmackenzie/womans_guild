d962570ed4c1721a24930b6d369a31b487b4fa02
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-08-12 16:57:50 +0300

    routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\YearController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/users',[UserController::class,'store']);
+    Route::get('/users',[UserController::class,'index']);
+
+    Route::apiResource('years',YearController::class);
 });
