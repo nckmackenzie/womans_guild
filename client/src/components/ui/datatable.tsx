794268f1c35f0a5b95a771ee40adc512515b0091
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-08-12 09:39:18 +0300

    feat: datatable component

---
+++ client/src/components/ui/datatable.tsx
@@ -0,0 +1,183 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client';
+
+import { useState } from 'react';
+import {
+  ColumnDef,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { DataTablePagination } from './datatable-pagination';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from './dropdown-menu';
+import { Button } from './button';
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  displayRowSelection?: boolean;
+  hasTotalsFooter?: boolean;
+  footerColspan?: number;
+  footerTotalValue?: string;
+  hasInvisibleColumns?: boolean;
+  columnsWithVisibilityStatus?: any;
+  defaultPageSize?: number;
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+  displayRowSelection,
+  footerColspan,
+  footerTotalValue,
+  hasTotalsFooter,
+  hasInvisibleColumns,
+  columnsWithVisibilityStatus,
+  defaultPageSize,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>(
+    columnsWithVisibilityStatus || {}
+  );
+  const [rowSelection, setRowSelection] = useState({});
+  const [pagination, setPagination] = useState({
+    pageIndex: 0, //initial page index
+    pageSize: defaultPageSize || 10, //default page size
+  });
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onRowSelectionChange: setRowSelection,
+    onColumnVisibilityChange: setColumnVisibility,
+    onPaginationChange: setPagination,
+    state: {
+      sorting,
+      rowSelection,
+      columnVisibility,
+      pagination,
+    },
+    initialState: {
+      columnVisibility: hasInvisibleColumns ? columnsWithVisibilityStatus : {},
+    },
+  });
+
+  return (
+    <div className="space-y-4">
+      {hasInvisibleColumns && (
+        <div className="flex justify-end">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="ml-auto">
+                Toggle visible columns
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {table
+                .getAllColumns()
+                .filter(column => column.getCanHide())
+                .map(column => {
+                  return (
+                    <DropdownMenuCheckboxItem
+                      key={column.id}
+                      className="capitalize"
+                      checked={column.getIsVisible()}
+                      onCheckedChange={value =>
+                        column.toggleVisibility(!!value)
+                      }
+                    >
+                      {column.id}
+                    </DropdownMenuCheckboxItem>
+                  );
+                })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      )}
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map(headerGroup => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map(header => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map(row => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map(cell => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+          {hasTotalsFooter && (
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={footerColspan}>Total</TableCell>
+                <TableCell className="text-right">{footerTotalValue}</TableCell>
+              </TableRow>
+            </TableFooter>
+          )}
+        </Table>
+      </div>
+      <DataTablePagination
+        table={table}
+        displayRowSelection={displayRowSelection}
+      />
+    </div>
+  );
+}
