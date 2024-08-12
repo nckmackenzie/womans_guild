61890b3d7ac644c55744fe5874602757a088722d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-08-12 16:48:55 +0300

    feat: migration for years table

---
+++ server/database/migrations/2024_08_12_110238_create_years_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('years', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('name')->unique();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->boolean('is_closed')->default(false);
+            $table->foreignId('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('years');
+    }
+};
