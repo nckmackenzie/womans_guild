a9f84b468aa17a06eb88ed27952f8b4cffc72216
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-12 22:19:58 +0300

    chore: type for Year

--- client/src/features/years/types.ts
+++ client/src/features/years/types.ts
@@ -3,3 +3,4 @@ import { z } from 'zod';
 import { yearFormSchema } from './schema';
 
 export type YearFormValues = z.infer<typeof yearFormSchema>;
+export type Year = YearFormValues & { id: string; is_closed: boolean };
