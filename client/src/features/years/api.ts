40aa6c464a2a60931bd0d79e27f9133d442dd771
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-12 22:12:09 +0300

    refactor: update and create from same function

--- client/src/features/years/api.ts
+++ client/src/features/years/api.ts
@@ -4,9 +4,13 @@ import axios from '@/lib/axios';
 import { Year, type YearFormValues } from './types';
 import { flattenErrors } from '@/lib/formatters';
 
-export async function createYear(values: YearFormValues) {
+export async function createYear(values: YearFormValues, id?: string) {
   try {
-    await axios.post('/api/years', values);
+    if (!id) {
+      await axios.post('/api/years', values);
+    } else {
+      await axios.patch(`/api/years/${id}`, values);
+    }
   } catch (error) {
     if (isAxiosError(error)) {
       const errors = flattenErrors(error.response?.data.errors).join('\n');
@@ -24,6 +28,17 @@ export async function fetchYears(query?: string): Promise<{ data: Year[] }> {
     const { data } = await axios.get(
       query ? `/api/years?search=${query}` : '/api/years'
     );
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('An error occurred while fetching data.');
+  }
+}
+
+export async function fetchYear(id: string): Promise<{ data: Year }> {
+  try {
+    const { data } = await axios.get(`/api/years/${id}`);
 
     return data;
   } catch (error) {
