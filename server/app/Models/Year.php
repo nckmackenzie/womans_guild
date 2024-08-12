1cc386a040358a5e330fe171bc6638cf4f8f44e0
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-12 19:56:20 +0300

    refactor: map cuid package

--- server/app/Models/Year.php
+++ server/app/Models/Year.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Parables\Cuid\CuidAsPrimaryKey;
 
 class Year extends Model
 {
     use HasFactory;
-    
+    use CuidAsPrimaryKey;
+
+
+    protected $keyType = 'string';
+    public $incrementing = false;
     protected $fillable = ['name','start_date','end_date','is_closed'];
 }
