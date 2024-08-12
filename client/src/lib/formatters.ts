f1b9bc5c2ee41067db7c8c5ac9b2f51586e6cdef
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-12 20:38:01 +0300

    chore: add formatter for date

--- client/src/lib/formatters.ts
+++ client/src/lib/formatters.ts
@@ -1,7 +1,13 @@
+import { format } from 'date-fns';
+
 export const flattenErrors = (error: Record<string, string[]>) => {
   return Object.values(error).flat();
 };
 
 export const titleCase = (str: string) => {
   return str.toLowerCase().replace(/\b\w/g, s => s.toUpperCase());
+};
+
+export const formatDateLong = (date: Date | string) => {
+  return format(new Date(date), 'PPP');
 };
