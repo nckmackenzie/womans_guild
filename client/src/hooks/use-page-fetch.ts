cb41ca5f25776080c38111fe95491d8f063506fa
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-15 22:06:52 +0300

    feat: custom hook to fetch page info

---
+++ client/src/hooks/use-page-fetch.ts
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
+
+export function usePageFetch<T>(
+  queryKey: string,
+  queryFn: (queryString?: string) => Promise<T>,
+  refetchInterval?: number
+) {
+  const [searchParams] = useSearchParams();
+  const queryString = searchParams.get('search') ?? undefined;
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: [queryKey, queryString],
+    queryFn: () => queryFn(queryString),
+    refetchInterval: refetchInterval ? refetchInterval : false,
+  });
+
+  return { isLoading, error, data };
+}
