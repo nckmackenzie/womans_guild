9e59d041c156f3e0a8731ab3baf96cce923daee3
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-15 22:38:55 +0300

    refactor: remove session checker

--- client/src/features/login/pages/login-page.tsx
+++ client/src/features/login/pages/login-page.tsx
@@ -1,22 +1,19 @@
 import { useTitle } from '@/hooks/use-title';
 import LoginForm from '../components/login-form';
-import { useAuth } from '@/hooks/use-auth';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
 
 export default function LoginPage() {
   useTitle('Login');
-  const navigate = useNavigate();
-  const { user } = useAuth({ middleware: 'auth' });
+  // const navigate = useNavigate();
+  // const { user } = useAuth({ middleware: 'auth' });
 
-  useEffect(
-    function () {
-      if (user) {
-        navigate('/dashboard');
-      }
-    },
-    [user, navigate]
-  );
+  // useEffect(
+  //   function () {
+  //     if (user) {
+  //       navigate('/dashboard');
+  //     }
+  //   },
+  //   [user, navigate]
+  // );
 
   return <LoginForm />;
 }
