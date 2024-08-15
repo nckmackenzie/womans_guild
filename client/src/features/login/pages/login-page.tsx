9a57a3f7d6e15592370b681c93398ea03577ac78
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-15 19:20:50 +0300

    feat: redirect user if session exists

--- client/src/features/login/pages/login-page.tsx
+++ client/src/features/login/pages/login-page.tsx
@@ -1,8 +1,22 @@
 import { useTitle } from '@/hooks/use-title';
 import LoginForm from '../components/login-form';
+import { useAuth } from '@/hooks/use-auth';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export default function LoginPage() {
   useTitle('Login');
+  const navigate = useNavigate();
+  const { user } = useAuth({ middleware: 'auth' });
+
+  useEffect(
+    function () {
+      if (user) {
+        navigate('/dashboard');
+      }
+    },
+    [user, navigate]
+  );
 
   return <LoginForm />;
 }
