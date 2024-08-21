50ab15a9dd2bc64f0120e7d62589d18892fc3c7c
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-21 20:07:35 +0300

    feat: add global middleware for case conversion

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ConvertCaseMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -19,6 +20,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
+
+        $middleware->append(ConvertCaseMiddleware::class);
 
         //
     })
