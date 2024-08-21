6d284bc6db4c04fc8680f68ca55acbfc6345cf79
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-21 22:31:39 +0300

    refactor: redirect incase of session

--- client/src/features/login/pages/login-page.tsx
+++ client/src/features/login/pages/login-page.tsx
@@ -1,19 +1,24 @@
-import { useTitle } from '@/hooks/use-title';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import LoginForm from '../components/login-form';
+
+import { useTitle } from '@/hooks/use-title';
+import { useAuth } from '@/hooks/use-auth';
 
 export default function LoginPage() {
   useTitle('Login');
-  // const navigate = useNavigate();
-  // const { user } = useAuth({ middleware: 'auth' });
+  const navigate = useNavigate();
+  const { user } = useAuth({ middleware: 'auth' });
 
-  // useEffect(
-  //   function () {
-  //     if (user) {
-  //       navigate('/dashboard');
-  //     }
-  //   },
-  //   [user, navigate]
-  // );
+  useEffect(
+    function () {
+      if (user) {
+        navigate('/dashboard');
+      }
+    },
+    [user, navigate]
+  );
 
   return <LoginForm />;
 }
