467d43bbfe829dc7efd67226b53de3d5e46b3285
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-22 21:12:29 +0300

    feat: a loader for pages

--- client/src/components/ui/loader.tsx
+++ client/src/components/ui/loader.tsx
@@ -12,3 +12,16 @@ export default function Loader({ loadingText }: { loadingText?: string }) {
     </div>
   );
 }
+
+export function PageLoader({ loadingText }: { loadingText?: string }) {
+  return (
+    <div className="h-[calc(100vh-64px)] flex items-center justify-center text-primary">
+      <div className="flex flex-col items-center space-y-1">
+        <LoaderIcon className="size-6 animate-spin" />
+        <p className="text-muted-foreground text-xs italic animate-pulse">
+          {loadingText || 'Loading page details...'}
+        </p>
+      </div>
+    </div>
+  );
+}
