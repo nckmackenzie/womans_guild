0d95fdb6955df10312ce1258a07f15ebcacafc89
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-22 16:31:25 +0300

    refactor: set loading text

--- client/src/components/ui/loader.tsx
+++ client/src/components/ui/loader.tsx
@@ -1,9 +1,14 @@
 import { LoaderIcon } from 'lucide-react';
 
-export default function Loader() {
+export default function Loader({ loadingText }: { loadingText?: string }) {
   return (
     <div className="h-[400px] flex items-center justify-center text-primary">
-      <LoaderIcon className="size-6 animate-spin" />
+      <div className="flex flex-col items-center space-y-1">
+        <LoaderIcon className="size-6 animate-spin" />
+        <p className="text-muted-foreground text-xs italic">
+          {loadingText || 'Please Wait...'}
+        </p>
+      </div>
     </div>
   );
 }
