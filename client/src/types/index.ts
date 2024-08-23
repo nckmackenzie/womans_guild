1b4dc9f0434f8688fcb0d1b82261b4e70f884097
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-23 15:06:13 +0300

    chore: types definition

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,2 +1,15 @@
 export type ServerError = Record<string, string[]>;
 export type Option = { value: string; label: string };
+export interface WithId {
+  id: string;
+}
+
+export interface WithName {
+  name: string;
+}
+
+export interface IdWithName extends WithId, WithName {}
+
+export interface IsEdit {
+  isEdit?: boolean;
+}
