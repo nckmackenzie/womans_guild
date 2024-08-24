c43674e562d2dda70bbd79c1351a482617fb2a45
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-24 08:04:32 +0300

    chore:initial page start up

---
+++ client/src/features/incomes/pages/income-page.tsx
@@ -0,0 +1,16 @@
+import ContentWrapper from '@/components/layout/content-wrapper';
+import { CreateNewButton } from '@/components/ui/button';
+import { DollarSign } from 'lucide-react';
+
+export default function IncomePage() {
+  return (
+    <ContentWrapper title="Incomes">
+      <div className="space-y-4">
+        <CreateNewButton href="/incomes/new">
+          <DollarSign className="icon mr-2" />
+          <span>Create new income</span>
+        </CreateNewButton>
+      </div>
+    </ContentWrapper>
+  );
+}
