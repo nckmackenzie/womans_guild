3feddb499f204998f25d7736634e46ffd9b612dc
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-27 21:21:16 +0300

    refactor: allow className to be passed

--- client/src/components/ui/form-group.tsx
+++ client/src/components/ui/form-group.tsx
@@ -1,3 +1,9 @@
-export default function FormGroup({ children }: { children: React.ReactNode }) {
-  return <div className="space-y-">{children}</div>;
+import { cn } from '@/lib/utils';
+import { PropsWithChildren } from 'react';
+
+export default function FormGroup({
+  children,
+  className,
+}: PropsWithChildren<{ className?: string }>) {
+  return <div className={cn('space-y-2', className)}>{children}</div>;
 }
