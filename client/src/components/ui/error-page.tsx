823ab4ecacb2fd859ef702b619ad82351f6b5546
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-31 16:00:29 +0300

    refactor: change ui for errors page

--- client/src/components/ui/error-page.tsx
+++ client/src/components/ui/error-page.tsx
@@ -1,25 +1,43 @@
-import { useRouteError } from 'react-router-dom';
-
-type CustomError = Error & {
-  statusText?: string;
-};
+import {
+  useRouteError,
+  isRouteErrorResponse,
+  Link,
+  useNavigate,
+} from 'react-router-dom';
+import { Card, CardContent, CardHeader, CardTitle } from './card';
+import { Button } from './button';
 
 export default function ErrorPage() {
-  const error = useRouteError() as CustomError;
+  const navigate = useNavigate();
+  const error = useRouteError();
   console.error(error);
 
+  let errorMessage: string;
+
+  if (isRouteErrorResponse(error)) {
+    errorMessage = error.statusText || 'Unknown error';
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else {
+    errorMessage = 'An unknown error occurred';
+  }
+
   return (
-    <div
-      id="error-page"
-      className="h-[400px] max-w-3xl mx-auto flex items-center"
-    >
-      <div className="flex flex-col items-center justify-center rounded-md border p-4">
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error?.statusText || error.message}</i>
-        </p>
-      </div>
+    <div id="error-page" className="grid h-screen place-content-center">
+      <Card>
+        <CardHeader className="p-4">
+          <CardTitle>😔An unexpected error has occured</CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <p className="text-sm text-muted-foreground">Error:{errorMessage}</p>
+          <div className="space-x-2">
+            <Button onClick={() => navigate(-1)}>Go Back</Button>
+            <Button variant={'secondary'} asChild>
+              <Link to="/dashboard">Go Home</Link>
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
