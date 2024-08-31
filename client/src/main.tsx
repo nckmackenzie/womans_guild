4bd9e9a5f0db404c304d7dee337ba1221c2bf80e
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-08-31 15:58:34 +0300

    refactor: change to using router provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AppProvider } from './app/app-provider.tsx';
-import App from '@/app/app.tsx';
+// import App from '@/app/app.tsx';
 import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/routes.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <RouterProvider router={router} />
     </AppProvider>
   </React.StrictMode>
 );
