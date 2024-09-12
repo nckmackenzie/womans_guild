e43195888bb7ef865136eeb523fa4730721502a7
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-09-12 20:42:49 +0300

    refactor: pass option to change file name

--- client/src/hooks/use-excel.ts
+++ client/src/hooks/use-excel.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { download, generateCsv, mkConfig } from 'export-to-csv';
 
-export function useExportExcel() {
+export function useExportExcel(fileName?: string) {
   const csvConfig = mkConfig({
     fieldSeparator: ',',
     decimalSeparator: '.',
     useKeysAsHeaders: true,
+    filename: fileName || 'generated',
   });
 
   function handleExport(data: any[]) {
