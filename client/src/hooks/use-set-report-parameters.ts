01949c4a6882dc8adf8c436743de588869d4ebbc
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-09-12 16:36:42 +0300

    refactor: remove keys through array

--- client/src/hooks/use-set-report-parameters.ts
+++ client/src/hooks/use-set-report-parameters.ts
@@ -2,13 +2,17 @@ import { useSearchParams } from 'react-router-dom';
 
 export function useSetReportParams() {
   const [searchParams, setSearchParams] = useSearchParams();
-  //   setSearchParams('');
-  return (object: object, initialObj: object) => {
-    for (const [key] of Object.entries(initialObj)) {
+  return (object: object, keys: string[]) => {
+    keys.forEach(key => {
       searchParams.delete(key);
-    }
+    });
+    // for (const [key] of Object.entries(initialObj)) {
+    //   searchParams.delete(key);
+    // }
     for (const [key, value] of Object.entries(object)) {
-      searchParams.set(key, value);
+      if (value !== undefined) {
+        searchParams.set(key, value);
+      }
     }
     setSearchParams(searchParams);
   };
