208e4edfcd4f15664a798e24fad37ea9022852fc
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-09-12 07:21:24 +0300

    style: add variant for excel

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -20,6 +20,8 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        excel:
+          'bg-emerald-400 transition-colors hover:bg-emerald-300 text-slate-900',
       },
       size: {
         default: 'h-10 px-4 py-2',
