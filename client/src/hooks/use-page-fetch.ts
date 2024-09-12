f8d36a570f11a222e1126712ac6539470b9c985b
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-09-12 07:13:46 +0300

    refactor: refactor for reports

--- client/src/hooks/use-page-fetch.ts
+++ client/src/hooks/use-page-fetch.ts
@@ -4,18 +4,20 @@ import { useSearchParams } from 'react-router-dom';
 export function usePageFetch<T>(
   queryKey: string,
   queryFn: (queryString?: string) => Promise<T>,
-  refetchInterval?: number
+  refetchInterval?: number,
+  report?: boolean
 ) {
   const [searchParams] = useSearchParams();
 
   // const queryString = searchParams.get('search') ?? undefined;
   const queryString = searchParams.toString() ?? undefined;
 
-  const { isLoading, error, data } = useQuery({
+  const { isLoading, error, data, refetch } = useQuery({
     queryKey: [queryKey, queryString],
     queryFn: () => queryFn(queryString),
     refetchInterval: refetchInterval ? refetchInterval : false,
+    enabled: report ? !!queryString : true,
   });
 
-  return { isLoading, error, data };
+  return { isLoading, error, data, refetch };
 }
