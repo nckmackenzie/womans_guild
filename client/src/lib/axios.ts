5ee3c003a35d2c72774b4879fd31d0430f73d6cd
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-09-18 16:41:55 +0300

    Update axios.ts

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 const axios = Axios.create({
   // baseURL: 'http://localhost:8000',
-  baseURL: import.meta.env.VITE_APP_BASE_URL,
+  baseURL: `${import.meta.env.VITE_APP_BASE_URL}/api`,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
   },
