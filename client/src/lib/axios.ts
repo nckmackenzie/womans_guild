4d272a33e1fbe5baa49cde16835c892d6c3040de
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-09-20 10:48:47 +0300

    send cookie

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,10 +1,12 @@
 import Axios from 'axios';
+import Cookies from 'js-cookie';
 
 const axios = Axios.create({
   // baseURL: 'http://localhost:8000',
   baseURL: import.meta.env.VITE_APP_BASE_URL,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
+    'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN') || '',
   },
   withCredentials: true,
   withXSRFToken: true,
