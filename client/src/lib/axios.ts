5ddf9d8fe4b012554706285ebc78251c2da6124d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-09-20 17:02:27 +0300

    refactor: add interceptors for axios

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,15 +1,30 @@
 import Axios from 'axios';
-import Cookies from 'js-cookie';
 
 const axios = Axios.create({
   // baseURL: 'http://localhost:8000',
   baseURL: import.meta.env.VITE_APP_BASE_URL,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
-    'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN') || '',
+
+    // 'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN') || '',
   },
   withCredentials: true,
   withXSRFToken: true,
 });
+
+axios.interceptors.request.use(
+  config => {
+    const bearerToken = localStorage.getItem('token');
+
+    if (bearerToken) {
+      config.headers['Authorization'] = `Bearer ${bearerToken}`;
+    }
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default axios;
