4c00f6f45db3aab8d6ca398ef6a6514005d8b01d
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-09-22 11:13:59 +0300

    chore: util function for setting user initials

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,6 +4,21 @@ import { twMerge } from 'tailwind-merge';
 import { flattenErrors } from './formatters';
 import { isAxiosError } from 'axios';
 
+export function getInitials(fullName: string | null): string {
+  if (!fullName) return 'U';
+
+  const names = fullName.trim().split(/\s+/);
+  if (names.length === 1) {
+    return names[0][0].toUpperCase();
+  } else if (names.length === 2) {
+    return names.map(name => name[0].toUpperCase()).join('');
+  } else {
+    return `${names[0][0].toUpperCase()}${names[
+      names.length - 1
+    ][0].toUpperCase()}`;
+  }
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
