0b286a114ad4044bf25ee21e706eab171807bed8
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-09-22 12:09:36 +0300

    refactor: add components for changing password

--- client/src/features/global/components/change-password-form.tsx
+++ client/src/features/global/components/change-password-form.tsx
@@ -8,12 +8,28 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { Form, FormField } from '@/components/ui/form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { PasswordInput } from '@/components/ui/password-input';
+import { Button } from '@/components/ui/button';
+import { ErrorComponent } from '@/components/ui/basic-alert';
+import { toast } from 'sonner';
 
 import type { ChangePasswordFormValues } from '@/features/global/types';
 import { changePasswordFormSchema } from '@/features/global/schema';
+import { useError } from '@/hooks/use-error';
+import { useMutate } from '@/hooks/use-mutate';
+import { changePassword } from '@/features/global/api';
 
 export default function ChangePasswordForm() {
+  const { clearErrors, errors, onError } = useError();
+  const { isPending, mutate } = useMutate(changePassword);
   const form = useForm<ChangePasswordFormValues>({
     defaultValues: {
       confirmPassword: '',
@@ -23,22 +39,100 @@ export default function ChangePasswordForm() {
     resolver: zodResolver(changePasswordFormSchema),
   });
 
-  function onSubmit(values: ChangePasswordFormValues) {}
+  function onSubmit(values: ChangePasswordFormValues) {
+    clearErrors();
+    mutate(values, {
+      onSuccess: () => {
+        form.reset();
+        toast.success('Password changed successfully');
+      },
+      onError: error => onError(error.message.split('\n')),
+    });
+  }
   return (
-    <Card className="max-w-lg mx-auto">
-      <CardHeader>
-        <CardTitle>Change Password</CardTitle>
-        <CardDescription>
-          Enter your old password and new password
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-            <FormField control={form.control} name="oldPassword" />
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+    <div className="space-y-4">
+      {errors && <ErrorComponent error={errors} />}
+      <Card className="max-w-lg mx-auto">
+        <CardHeader>
+          <CardTitle>Change Password</CardTitle>
+          <CardDescription>
+            Enter your old password and new password
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="oldPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Old Password</FormLabel>
+                    <FormControl>
+                      <PasswordInput
+                        {...field}
+                        placeholder="Your old password"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="newPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>New Password</FormLabel>
+                    <FormControl>
+                      <PasswordInput
+                        {...field}
+                        placeholder="Your new password"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <PasswordInput
+                        {...field}
+                        placeholder="Confirm new password"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="space-x-2">
+                <Button type="submit" disabled={isPending}>
+                  Submit
+                </Button>
+                <Button
+                  disabled={isPending}
+                  type="reset"
+                  variant="outline"
+                  onClick={() => {
+                    form.reset();
+                    clearErrors();
+                  }}
+                >
+                  Reset
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
