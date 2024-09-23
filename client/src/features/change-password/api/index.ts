7b409188f0ce34ef0ea4dd34ac3bb2e0120e8828
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-09-23 22:08:30 +0300

    refactor: wont return

--- client/src/features/change-password/api/index.ts
+++ client/src/features/change-password/api/index.ts
@@ -1,13 +1,9 @@
 import axios from '@/lib/axios';
 import { handleMutationError } from '@/lib/utils';
 
-export async function forgotPassword(values: {
-  contact: string;
-}): Promise<{ message: string; token: string } | undefined> {
+export async function forgotPassword(values: { contact: string }) {
   try {
-    const { data } = await axios.post('/api/forgotPassword', values);
-
-    return data;
+    await axios.post('/api/forgotPassword', values);
   } catch (error) {
     handleMutationError(error);
   }
