63559c5b68b029b165b5b9f66ba0a19b5863a3b9
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-09-23 16:58:57 +0300

    chore: remove schema file

--- client/src/features/change-password/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const forgotPasswordSchema = z.object({
-  phoneNumber: z
-    .string()
-    .min(1, 'Phone number is required')
-    .max(10, 'Invalid phone number'),
-});
