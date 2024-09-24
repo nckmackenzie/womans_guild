9ed30a8962443ed5b09d8c258c115e5150dc42eb
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-09-24 08:23:53 +0300

    chore: add type for IncomeProjectionTableRow

--- client/src/features/income-projection/types/index.ts
+++ client/src/features/income-projection/types/index.ts
@@ -1,6 +1,12 @@
 import { z } from 'zod';
 import { incomeProjectionsSchema } from '@/features/income-projection/schema';
+import { WithId } from '@/types';
 
 export type IncomeProjectionsFormValues = z.infer<
   typeof incomeProjectionsSchema
 >;
+
+export interface IncomeProjectionTableRow extends WithId {
+  yearName: string;
+  totalAmount: number;
+}
