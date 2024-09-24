63cc77bbd1a65faab7de1394d61e4059516adff0
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-09-24 09:18:59 +0300

    refactor: edit and update

--- client/src/features/income-projection/pages/create-edit.tsx
+++ client/src/features/income-projection/pages/create-edit.tsx
@@ -8,20 +8,33 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import CreateEditForm from '@/features/income-projection/components/create-edit-form';
-import { useFetchVoteheads } from '@/features/voteheads/hooks/use-fetch-voteheads';
+import { ErrorComponent } from '@/components/ui/basic-alert';
+import { PageLoader } from '@/components/ui/loader';
 
+import { useFetchVoteheads } from '@/features/voteheads/hooks/use-fetch-voteheads';
+import { useFetchSingle } from '@/hooks/use-fetch-single';
 import { useTitle } from '@/hooks/use-title';
 import type { IsEdit } from '@/types';
+import { fetchIncomeProjection } from '@/features/income-projection/api';
 
 export default function CreateEditPage({ isEdit }: IsEdit) {
   useTitle(isEdit ? 'Edit Income Projection' : 'Create Income Projection');
   const { voteheads } = useFetchVoteheads('INCOME');
+  const { data, error, isLoading } = useFetchSingle(
+    'income-projection',
+    fetchIncomeProjection
+  );
+
+  if (isLoading)
+    return <PageLoader loadingText="Fetching income projection details..." />;
+
   return (
     <ContentWrapper
       title={isEdit ? 'Edit Income Projection' : 'Create Income Projection'}
     >
       <div className="space-y-4">
         <BackButton />
+        {error && <ErrorComponent error={error.message} />}
         <Card>
           <CardHeader>
             <CardTitle>
@@ -32,7 +45,11 @@ export default function CreateEditPage({ isEdit }: IsEdit) {
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <CreateEditForm isEdit={!!isEdit} voteheads={voteheads} />
+            <CreateEditForm
+              isEdit={!!isEdit}
+              voteheads={voteheads}
+              data={data?.data}
+            />
           </CardContent>
         </Card>
       </div>
